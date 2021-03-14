9c0f5b5bce9b30f9763643635095ee2e3115f625
SimonLarsson91 <simonlarsson09@gmail.com>
2021-03-14 16:30:49 +0100

    fix

--- generate.py
+++ generate.py
@@ -1,5 +1,7 @@
 import numpy
 import random
+import sys
+sys.setrecursionlimit(100000)
 
 def jacobi_symbol(a,b):
     a, b = int(a), int(b)
@@ -12,7 +14,10 @@ def jacobi_symbol(a,b):
             return jacobi_symbol(b % a, a) * (-1)**((a-1)*(b-1)/4)
 
 def generate_prime_candidate(n_size):
-    return random.getrandbits(n_size)
+    n = random.getrandbits(n_size)
+    while n < 3:
+        n = random.getrandbits(n_size)
+    return n
 
 def gcd(a,b):
     while not a == b :
@@ -21,21 +26,13 @@ def gcd(a,b):
         else :
             b = b - a
     return a
-
-def random_number_not_zero(b):
-    a = random.randrange(b)
-    while a == 0:
-        a = random.randrange(b)
-    return a
         
-
 def generate_prime(n_size):
-    b = 13##generate_prime_candidate(n_size)
+    b = generate_prime_candidate(n_size)
     success = True
-    for i in range(3):
-        a = random_number_not_zero(b)
-
-        if gcd(a, b) == 1 and jacobi_symbol(a, b) == a*(b-1)/2 % b:
+    for i in range(10):
+        a = random.randrange(1,b)
+        if gcd(a, b) == 1 and jacobi_symbol(a, b) == a**((b-1)/2) % b:
             continue
         else:
             success = False
