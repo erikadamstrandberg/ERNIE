05147671220db6d8ab7fc6f9593d96b88fcab86d
erikadamstrandberg <erikadamstrandberg@gmail.com>
2021-03-14 14:43:06 +0100

    Made argparser not need arguments for rsa_key

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import argparse
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser('rsa encrypt')
-    parser.add_argument("--rsa_key", required=False, default=False, type=bool, help="generate new key pair")
+    parser.add_argument("--rsa_key", required=False, action='store_true', help="generate new key pair")
     parser.add_argument("--encrypt", required=False, default=False, type=bool, help="encrypt a file")
     args = parser.parse_args()
 
     if args.rsa_key:
         # generate a rsa key (public private pair)
-        pass
+        print("No argument needed!")
+
     if args.encrypt:
         new_parser = argparse.ArgumentParser('encryption set up')
         new_parser.add_argument("--public_key", required=True, default=None, type=str, help="public key to use for encryption")
