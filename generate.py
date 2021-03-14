7f447575bd7e55b66f036964815b5f0651b66f16
SimonLarsson91 <simonlarsson09@gmail.com>
2021-03-14 15:59:35 +0100

    jacobi fix

--- generate.py
+++ generate.py
@@ -5,10 +5,11 @@ def jacobi_symbol(a,b):
     a, b = int(a), int(b)
     if a == 1:
         return 1
-    elif a % 2 ==0:
-        return jacobi_symbol(a/2,b) * (-1)**((b**2 -1)/8)
     else:
-        return jacobi_symbol(b % a, a) * (-1)**((a-1)*(b-1)/4)
+        if a % 2 ==0:
+            return jacobi_symbol(a/2,b) * (-1)**((b**2 -1)/8)
+        else:
+            return jacobi_symbol(b % a, a) * (-1)**((a-1)*(b-1)/4)
 
 def generate_prime_candidate(n_size):
     return random.getrandbits(n_size)
@@ -23,6 +24,7 @@ def gcd(a,b):
 
 def generate_prime(n_size):
     b = generate_prime_candidate(n_size)
+    #print(b)
     success = True
     for i in range(100):
         a = random.randrange(b)
