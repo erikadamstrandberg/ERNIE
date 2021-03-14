464c69aec7a8ef55e9d501dcc38eb2bceeca490b
SimonLarsson91 <simonlarsson09@gmail.com>
2021-03-14 15:49:59 +0100

    prime gen added

--- generate.py
+++ generate.py
@@ -21,3 +21,30 @@ def gcd(a,b):
             b = b - a
     return a
 
+def generate_prime(n_size):
+    b = generate_prime_candidate(n_size)
+    success = True
+    for i in range(100):
+        a = random.randrange(b)
+        if gcd(a, b) == 1 and jacobi_symbol(a, b) == a*(b-1)/2 % b:
+            continue
+        else:
+            success = False
+            break
+    if success:
+        return b
+    else:
+        return generate_prime(n_size)
+    
+class RsaKeyPair():
+    def __init__(self,n_size=100):
+        self.p = 1
+        self.q = 1
+        self.d = 1
+        self.e = 1
+        self.n_size = n_size
+    
+
+
+            
+        
